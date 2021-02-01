a1ee2456705fa821c482898d230f346f03d3a660
Edwin <woodmouth@gmail.com>
2021-02-01 20:11:54 +0800

    增加设置代理的脚本

---
+++ set_proxy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export HTTP_PROXY=http://127.0.0.1:8118
+export HTTPS_PROXY=http://127.0.0.1:8118
+export http_proxy=http://127.0.0.1:8118
+export https_proxy=http://127.0.0.1:8118
+
---
+++ unset_proxy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset http_proxy
+unset https_proxy
+
